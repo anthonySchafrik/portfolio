9749ea91a5b4f71331f8ce4500942bb4f5606419
Anthony Schafrik <anthonyschafrik@gmail.com>
2020-01-05 16:19:27 -0500

    Work page css done

--- src/Components/WorkPage.js
+++ src/Components/WorkPage.js
@@ -1,7 +1,75 @@
 import React from 'react';
 
+import pic from '../photos/work_portrait_clear.png';
+import './WorkPage.css';
+
 const WorkPage = () => {
-  return <div>This is the Work page component</div>;
+  return (
+    <div className="workContainer">
+      <div style={{ marginLeft: '3vw' }}>
+        <div
+          className="appInfoBoxes"
+          style={{
+            width: '60vw'
+          }}
+        >
+          1
+        </div>
+        <div
+          className="appInfoBoxes"
+          style={{
+            width: '58vw'
+          }}
+        >
+          2
+        </div>
+        <div
+          className="appInfoBoxes"
+          style={{
+            width: '56vw'
+          }}
+        >
+          3
+        </div>
+        <div
+          className="appInfoBoxes"
+          style={{
+            width: '54vw'
+          }}
+        >
+          4
+        </div>
+        <div
+          className="appInfoBoxes"
+          style={{
+            width: '52vw'
+          }}
+        >
+          15
+        </div>
+        <div
+          className="appInfoBoxes"
+          style={{
+            width: '50vw'
+          }}
+        >
+          6
+        </div>
+        <div
+          className="appInfoBoxes"
+          style={{
+            width: '52vw'
+          }}
+        >
+          7
+        </div>
+      </div>
+
+      <div className="workPicContainer">
+        <img src={pic} alt="Me in suite" height="700px" width="500px" />
+      </div>
+    </div>
+  );
 };
 
 export default WorkPage;
